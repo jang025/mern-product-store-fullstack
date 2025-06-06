05a39125ee8afd9ba7dc1c24ea4f4935adc6bc4e
Jeremy Ang <jang025@users.noreply.github.com>
2025-06-06 22:37:21 +0800

    Deployed to Production

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,29 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoute from "./routes/productRoute.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 //console.log(process.env.MONGO_URI);
 
+const __dirname = path.resolve();
+
 // middleware to parse the user's data
 app.use(express.json());
 
 // product route
 app.use("/api/product", productRoute);
+
+// in production , serve the frontend react files as a static asset
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
